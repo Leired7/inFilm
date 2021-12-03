87c47a7f95df36e6ac4474c746e41d2fb9edf515
Leired7 <leirediez.7@gmail.com>
2021-12-03 19:15:47 +0100

    Refactor: Crea describe para los primeros test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,20 +3,22 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import popularFilms from './mocks/popular_movie.json';
 
-test('No muestra las 50 pelis más vistas', () => {
-  render(<App />);
-  const dontShowMessage = screen.getByText(
-    /no se han podido mostrar las 50 películas más vistas/i
-  );
-  expect(dontShowMessage).toBeInTheDocument();
-});
+describe('Historia de usuarie 1: "poder ver la portada de las 20 pelis más vistas para elegir pelis para echar la siesta"', () => {
+  test('No muestra las 50 pelis más vistas', () => {
+    render(<App />);
+    const dontShowMessage = screen.getByText(
+      /no se han podido mostrar las 50 películas más vistas/i
+    );
+    expect(dontShowMessage).toBeInTheDocument();
+  });
 
-test('Muestra la carátula de las 20 películas más vistas', async () => {
-  render(<App />);
+  test('Muestra la carátula de las 20 películas más vistas', async () => {
+    render(<App />);
 
-  for (let film of popularFilms.results) {
-    await screen.findByRole('img', {
-      name: `${film.title}`,
-    });
-  }
+    for (let film of popularFilms.results) {
+      await screen.findByRole('img', {
+        name: `${film.title}`,
+      });
+    }
+  });
 });
