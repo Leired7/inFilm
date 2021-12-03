46b958178c96d0b1b1f3a7d0b2cba0cc2ba97fa3
Leired7 <leirediez.7@gmail.com>
2021-12-03 18:17:03 +0100

    Feat: Añade la carátula de una película desde el json del service worker

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
+export interface infoFromFilm {
+  backdrop_path: string;
+  title: string;
+}
 function App() {
-  return <p>No se han podido mostrar las 50 películas más vistas</p>;
+  const [filmsInfo, setFilmInfo] = React.useState<Array<infoFromFilm>>([]);
+  const [loading, setLoading] = React.useState<boolean>(false);
+  const [error, setError] = React.useState<boolean>(false);
+
+  useEffect(() => {
+    setLoading(true);
+    try {
+      const popularFilms = async () => {
+        const response = await fetch(
+          `https://api.themoviedb.org/3/movie/popular`
+        );
+
+        const data = await response.json();
+        setFilmInfo(data.results);
+      };
+      popularFilms();
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  return (
+    <>
+      {!loading && filmsInfo.length > 0 && (
+        <img
+          src={`https://image.tmdb.org/t/p/w500/${filmsInfo[0].backdrop_path}`}
+          alt={filmsInfo[0].title}
+        />
+      )}
+      <p>No se han podido mostrar las 50 películas más vistas</p>
+    </>
+  );
 }
 
 export default App;
