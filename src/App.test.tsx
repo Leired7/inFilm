a0cf3c47952a8224494aa2095a46f864d34c013a
Leired7 <leirediez.7@gmail.com>
2021-12-04 08:58:18 +0100

    Test: Añade pruebas para los estados cargando y error cuando conseguimos info de pelis

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 import popularFilms from './mocks/popular_movie.json';
+import { server } from './mocks/server';
+import { rest } from 'msw';
 
 describe('Historia de usuarie 1: "poder ver la portada de las 20 pelis más vistas para elegir pelis para echar la siesta"', () => {
-  test('No muestra las 50 pelis más vistas', () => {
+  test('Mientras cargan las imágenes se muestra un mensaje de "Cargando..."', () => {
     render(<App />);
-    const dontShowMessage = screen.getByText(
-      /no se han podido mostrar las 50 películas más vistas/i
-    );
-    expect(dontShowMessage).toBeInTheDocument();
-  });
 
+    const loadingMessage = /Cargando.../i;
+
+    screen.queryByText(loadingMessage);
+  });
+  test('No muestra las 50 pelis más vistas', () => {
+    server.use(
+      rest.get(
+        'https://api.themoviedb.org/3/movie/popular',
+        (req, res, ctx) => {
+          return res(ctx.status(500), ctx.json({ error: 'Server Error' }));
+        }
+      )
+    );
+
+    render(<App />);
+
+    const dontShowMessage =
+      /no se han podido mostrar las 50 películas más vistas/i;
+
+    screen.queryByText(dontShowMessage);
+  });
   test('Muestra la carátula de las 20 películas más vistas', async () => {
     render(<App />);
 
