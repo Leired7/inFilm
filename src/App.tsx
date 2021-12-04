0bf57722a44f7f7f9017de78be7eb6801b84ec0a
Leired7 <leirediez.7@gmail.com>
2021-12-04 12:25:20 +0100

    Chore: Elimina elemento importado no usado

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { SyntheticEvent, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { HomeContainer } from './components/HomeContainer/Home';
 
