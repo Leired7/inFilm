6c171da4024001575cc5cb2ebba6fd0d3b1cb2b3
Leired7 <leirediez.7@gmail.com>
2021-12-05 15:14:32 +0100

    Refacor: Extrae de llamada a los datos de la api

---
+++ src/core/infraestructure/Api/Api.ts
@@ -0,0 +1,10 @@
+import { InfoFromFilm } from '../../dominio/model';
+
+export const Api = {
+  getAllFilms: async (): Promise<InfoFromFilm[]> => {
+    const response = await fetch(`https://api.themoviedb.org/3/movie/popular`);
+    const data = await response.json();
+
+    return data.results;
+  },
+};
