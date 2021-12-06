a0b2187fcb1374ca6e3a8e1874730337e7101bf4
Leired7 <leirediez.7@gmail.com>
2021-12-06 14:38:24 +0100

    Fix-test: Añadidos atributos al renderizar <Navigation/>

--- src/ui/components/Navigation/__test__/Navigation.test.tsx
+++ src/ui/components/Navigation/__test__/Navigation.test.tsx
@@ -5,7 +5,15 @@ import { Navigation } from '../Navigation';
 
 describe('Comprueba que están todos los elementos', () => {
   it('Tiene que aparecer el Logo', () => {
-    render(<Navigation />);
+    render(
+      <Navigation
+        textToFilter={''}
+        setTextToFilter={function (text: string): void {
+          throw new Error('Function not implemented.');
+        }}
+        formatedFilter={''}
+      />
+    );
 
     screen.getByRole('img', { name: 'Logo de inFilm' });
   });
