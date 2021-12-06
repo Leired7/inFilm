83bebbc9d6aa0750e50a060920b5ee2d51902b14
Leired7 <leirediez.7@gmail.com>
2021-12-06 10:49:22 +0100

    Chore: Añade process.env.REACT_APP_THEMOVIEDB_API_KEY

--- src/core/infraestructure/Api/Api.ts
+++ src/core/infraestructure/Api/Api.ts
@@ -2,7 +2,9 @@ import { InfoFromFilm } from '../../domain/model';
 
 export const Api = {
   getAllFilms: async (): Promise<InfoFromFilm[]> => {
-    const response = await fetch(`https://api.themoviedb.org/3/movie/popular`);
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?${process.env.REACT_APP_THEMOVIEDB_API_KEY}&language=es`
+    );
     const data = await response.json();
 
     return data.results;
