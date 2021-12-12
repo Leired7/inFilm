d062bd3cb6357b0ca859d8413855312af55f1375
Leired7 <leirediez.7@gmail.com>
2021-12-12 08:37:33 +0100

    Refactor: Extrae BrowserRouter al index de la aplicación

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -20,7 +21,9 @@ async function main() {
 
   ReactDOM.render(
     <React.StrictMode>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
   );
