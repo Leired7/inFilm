ad1e61d592d9b64ebabc6f594d4f3e3a8e36913b
Leired7 <leirediez.7@gmail.com>
2021-12-13 18:58:00 +0100

    Feat: Muestra el título de la película y el resumen

--- src/ui/components/FilmCardInformation/FilmCardInformation.tsx
+++ src/ui/components/FilmCardInformation/FilmCardInformation.tsx
@@ -1,13 +1,70 @@
-import { useParams } from 'react-router-dom';
+import { url } from 'inspector';
+import { Link, useLocation } from 'react-router-dom';
+import { InfoFromFilm } from 'src/core/domain/model';
+import { color, font, grid, media, sizes } from 'src/ui/theme';
+import styled from 'styled-components';
 
 export const FilmCardInformation = () => {
-  const filmId = useParams();
+  const location = useLocation();
+  const cardInfo: InfoFromFilm = location.state.filmCardInfo;
+
   return (
     <>
-      <h1>
-        Soy el componente FilmCardInformation de la información de la películas:{' '}
-        {filmId.filmId}
-      </h1>
+      <BackgroundImage
+        style={{
+          backgroundImage: `url(https://image.tmdb.org/t/p/w500${cardInfo.backdrop_path})`,
+        }}
+      ></BackgroundImage>
+      <TextWrapper>
+        <Title>
+          {cardInfo.title} <Highlight>{cardInfo.release_date}</Highlight>
+        </Title>
+        <Overview>{cardInfo.overview}</Overview>
+      </TextWrapper>
     </>
   );
 };
+
+const BackgroundImage = styled.div`
+  width: 100%;
+  height: 300px;
+  background-size: cover;
+  background-repeat: no-repeat;
+`;
+
+const Title = styled.h1`
+  color: white;
+  font-family: 'Londrina Solid';
+  font-size: ${font.sizes.h1};
+  font-weight: ${font.weight.black};
+
+  margin-bottom: ${sizes.small};
+
+  ${media.tablet`
+    margin: 0;
+  `}
+`;
+
+const Highlight = styled.span`
+  color: ${color.golden};
+  font-weight: ${font.weight.bold};
+  font-size: ${font.sizes.medium};
+`;
+
+const Overview = styled.p`
+  color: ${color.white};
+`;
+
+const TextWrapper = styled.div`
+  padding: ${grid.gap.desktop}px;
+`;
+
+const StyledLink = styled(Link)`
+  display: inline-block;
+  text-transform: uppercase;
+  letter-spacing: 0.02rem;
+  font-family: 'Londrina Solid';
+  font-size: ${font.sizes.base};
+  color: ${color.golden};
+  margin: ${grid.gap.desktop}px 0 0 0;
+`;
