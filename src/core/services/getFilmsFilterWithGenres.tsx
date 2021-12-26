87a5b99544365facc5cf46ab527d61f1bea48a0a
Leired7 <leirediez.7@gmail.com>
2021-12-26 16:22:41 +0100

    Refactor: Extrar lógica en el tratamiento del texto que introduce el usuarie

--- src/core/services/getFilmsFilterWithGenres.tsx
+++ src/core/services/getFilmsFilterWithGenres.tsx
@@ -1,23 +1,16 @@
 import { InfoFromFilm } from '../domain/model';
-
 import { getGenresFromIds } from './getGenresFromIds';
 
 export const getFilmsFilterwithGenres = (
   userTypeString: string,
   fetchedFilms: InfoFromFilm[]
 ) => {
-  const formatedFilter: string = userTypeString
-    .split(' ')
-    .filter((substring: string) => substring)
-    .join(' ')
-    .toLowerCase();
-
   const filteredFilms: InfoFromFilm[] = fetchedFilms.filter(
     (item: InfoFromFilm, index: number) => {
       const minimumCaractersToSearch = 3;
 
-      if (formatedFilter.length >= minimumCaractersToSearch) {
-        return item.title.toLowerCase().includes(formatedFilter);
+      if (userTypeString.length >= minimumCaractersToSearch) {
+        return item.title.toLowerCase().includes(userTypeString);
       }
       return item;
     }
@@ -25,5 +18,5 @@ export const getFilmsFilterwithGenres = (
 
   const filmsWithGenres = getGenresFromIds(filteredFilms);
 
-  return { filmsWithGenres, formatedFilter };
+  return filmsWithGenres;
 };
